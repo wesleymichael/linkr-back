6c894dded416657cd8babca925af3ab8dab330a9
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-05-29 22:46:23 -0300

    feat: database connection

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import router from "./routers/index.router.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(router)
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
