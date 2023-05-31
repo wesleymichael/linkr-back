89b73ced9ec045fac624dd2026c9e66b04b9c76b
Lúcio Bruno <luciobrunop20@hotmail.com>
2023-05-31 04:53:30 -0400

    feat/GET-user-by-id

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,12 +1,42 @@
-import { searchUsersByName } from "../repository/users.repository.js";
+import { getPostByUserIdDB } from "../repository/posts.repository.js";
+import { getUsersByIdDB, searchUsersByNameDB } from "../repository/users.repository.js";
 
-export async function searchUsers(req,res){
+export async function searchUsers(req, res) {
 
     const { name } = req.body
 
     try {
-        const users = await searchUsersByName(name)
-        res.send(users.rows);        
+        const users = await searchUsersByNameDB(name)
+
+        for (let i = 0; i < users.rowCount; i++) {
+            delete users.rows[i].password
+            delete users.rows[i].email
+        }
+
+        res.send(users.rows);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+export async function userById(req, res) {
+
+    const { id } = req.params
+
+    try {
+        const user = await getUsersByIdDB(id)
+        const posts = getPostByUserIdDB(id)
+
+        if (user.rowCount === 0) {
+            return res.sendStatus(404)
+        }
+
+        delete user.rows[0].password
+        delete user.rows[0].email
+
+        const userAndPosts = { user: user.rows[0], posts: posts.rows }
+
+        res.send(userAndPosts);
     } catch (error) {
         res.status(500).send(error.message);
     }
