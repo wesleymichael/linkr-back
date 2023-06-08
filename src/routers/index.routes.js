7c3808c204cb7151d44d11d190c281a48ee2984f
Lúcio Bruno <luciobrunop20@hotmail.com>
2023-06-08 04:11:19 -0400

    feat/add comment and get comments

--- src/routers/index.routes.js
+++ src/routers/index.routes.js
@@ -3,11 +3,13 @@ import hashtagsRouter from "./hashtag.routes.js";
 import postsRouter from "./posts.routes.js";
 import usersRouter from "./users.routes.js";
 import loginRouter from "./login.routes.js";
+import commentsRouter from "./comments.routes.js";
 
 const router = Router();
 router.use(postsRouter);
 router.use(usersRouter);
 router.use(hashtagsRouter);
 router.use(loginRouter)
+router.use(commentsRouter)
 
 export default router;
